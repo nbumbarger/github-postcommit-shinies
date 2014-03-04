644668799403ea7df51e2dfff5545787ec5b8ca0
Nelson Pecora <nelson@yoshokatana.com>
2014-03-04 10:34:25 -0500

    added label toggle/add/remove, updated all functions to github api v3, added milestone updating

--- gh.rb
+++ gh.rb
@@ -11,38 +11,68 @@ set :logging, true
 set :port, 3000
 set :gh_user, config['user']
 set :gh_token, config['token']
-set :gh_api, "https://github.com/api/v2/json/"
-set :gh_apiv3, "https://api.github.com/"
-set :gh_issue, "issues/show/:user/:repo/:number"
-set :gh_add_label, "issues/label/add/:user/:repo/:label/:number"
-set :gh_edit_issue, "repos/:user/:repo/issues/:number"
-#view_issue = "issues/show/dmsfl/fleet/23"
+set :gh_api, "https://api.github.com/"
+set :gh_issue, "repos/:owner/:repo/issues/:number/labels" # get
+set :gh_add_label, "repos/:owner/:repo/issues/:number/labels" # post
+set :gh_remove_label, "repos/:owner/:repo/issues/:number/labels/:name" # delete
+set :gh_update_label, "repos/:owner/:repo/issues/:number/labels" # put
+set :gh_edit_issue, "repos/:owner/:repo/issues/:number"
 
 
 def get_labels(user, repo, issue)
-    endpoint = options.gh_issue.gsub(':user', user).gsub(':repo', repo).gsub(':number', issue)
-    c = Curl::Easy.new(options.gh_api + endpoint)
-    c.http_auth_types = :basic
-    c.username = options.gh_user
-    c.password = options.gh_token
-    c.perform
-    json = JSON.parse(c.body_str)
+    endpoint = options.gh_issue.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue)
+    curl = Curl::Easy.http_get(options.gh_api + endpoint) do |c|
+        c.http_auth_types = :basic
+        c.username = options.gh_user
+        c.password = options.gh_token
+    end
+    json = JSON.parse(curl.body_str)
     json['issue']['labels']
 end
 
-def add_label(user, repo, issue, label)
-    endpoint = options.gh_add_label.gsub(':user', user).gsub(':repo', repo).gsub(':number', issue).gsub(':label', label)
-    c = Curl::Easy.new(options.gh_api + endpoint)
-    c.http_auth_types = :basic
-    c.username = options.gh_user
-    c.password = options.gh_token
-    c.perform
-    p c.body_str
+def add_labels(user, repo, issue, label)
+    endpoint = options.gh_add_label.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue)
+    curl = Curl::Easy.http_post(options.gh_api + endpoint, label) do |c|
+        c.http_auth_types = :basic
+        c.username = options.gh_user
+        c.password = options.gh_token
+    end
+    p curl.body_str
+end
+
+def remove_label(user, repo, issue, label)
+    endpoint = options.gh_remove_label.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue).gsub(':name', label)
+    curl = Curl::Easy.http_delete(options.gh_api + endpoint) do |c|
+        c.http_auth_types = :basic
+        c.username = options.gh_user
+        c.password = options.gh_token
+    end
+    p curl.body_str
+end
+
+def update_labels(user, repo, issue, label)
+    endpoint = options.gh_update_label.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue)
+    curl = Curl::Easy.http_put(options.gh_api + endpoint, label) do |c|
+        c.http_auth_types = :basic
+        c.username = options.gh_user
+        c.password = options.gh_token
+    end
+    p curl.body_str
 end
 
 def assign_issue(user, repo, issue, assignee)
-    endpoint = options.gh_edit_issue.gsub(':user', user).gsub(':repo', repo).gsub(':number', issue)
-    curl = Curl::Easy.http_post(options.gh_apiv3 + endpoint,{:assignee => assignee}.to_json) do |c|
+    endpoint = options.gh_edit_issue.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue)
+    curl = Curl::Easy.http_patch(options.gh_api + endpoint, {:assignee => assignee}.to_json) do |c|
+        c.http_auth_types = :basic
+        c.username = options.gh_user
+        c.password = options.gh_token
+    end
+    p curl.body_str
+end
+
+def assign_milestone(user, repo, issue, milestone)
+    endpoint = options.gh_edit_issue.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue)
+    curl = Curl::Easy.http_patch(options.gh_api + endpoint, {:milestone => milestone}.to_json) do |c|
         c.http_auth_types = :basic
         c.username = options.gh_user
         c.password = options.gh_token
@@ -61,17 +91,47 @@ post '/' do
     push['commits'].each do |c|
         m = c['message']
         issue = m.scan(/[^\#][0-9]+/)
-            if issue.size == 1 #only check for other goodies if an issue is mentioned
-                begin
-                    user = m.scan(/\=[a-zA-Z0-9]+/)[0].split(//)[1..-1].join
-                    assign_issue(owner, repo, issue[0], user)
-                rescue => e
-                  p e.to_s
-                end
-                labels = m.scan(/\~[a-zA-Z0-9]+/)
-                labels.each do |l|
-                    add_label(owner, repo, issue[0], l.gsub('~', ''))
-                end
+        if issue.size == 1 #only check for other goodies if an issue is mentioned
+            
+            # user assignment
+            begin
+                user = m.scan(/\=[a-zA-Z0-9]+/)[0].split(//)[1..-1].join
+                assign_issue(owner, repo, issue[0], user)
+            rescue => e
+              p e.to_s
             end
+            
+            # label toggles
+            label_toggles = m.scan(/\~[a-zA-Z0-9]+|\~".*?"|\~'.*?'/)
+            if label_toggles.size > 0
+                current_labels = get_labels()
+
+                label_toggles.map { |s| s[1..-1] } # remove beginning ~
+
+                label_toggles.reject { |l| current_labels.any? { |x| x[:name] == l } } # remove strings from label_toggles that match the name of any hash in current_labels
+
+                update_label(owner, repo, issue[0], label_toggles)
+            end
+
+            # label adding
+            label_additions = m.scan(/\+[a-zA-Z0-9]+|\+".*?"|\+'.*?'/)
+            if label_additions.size > 0
+                add_labels(owner, repo, issue[0], label_additions)
+            end
+
+            # label removing
+            label_deletions = m.scan(/\-[a-zA-Z0-9]+|\-".*?"|\-'.*?'/)
+            if label_deletions.size > 0
+                remove_labels(owner, repo, issue[0], label_deletions)
+            end
+
+            # milestone assignment
+            begin
+                milestone = m.scan(/\^[a-zA-Z0-9]+/)[0].split(//)[1..-1].join
+                assign_milestone(owner, repo, issue[0], milestone)
+            rescue => e
+              p e.to_s
+            end
+        end
     end
 end
