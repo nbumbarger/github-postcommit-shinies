e96d42c8469719e88b11cf0b75ec84d78a0a5687
Nelson Pecora <nelson@yoshokatana.com>
2014-03-04 14:01:37 -0500

    fixed a bunch of errors

--- gh.rb
+++ gh.rb
@@ -5,8 +5,10 @@ require 'curb'
 require 'erubis'
 
 begin
+    # local
     config = YAML::load(Erubis::Eruby.new(File.open('config.yml').read).result)
 rescue
+    # heroku
     config = YAML::load(Erubis::Eruby.new(File.open('config.yml.heroku_sample').read).result)
 end
 
@@ -20,7 +22,7 @@ set :gh_issue, "repos/:owner/:repo/issues/:number/labels" # get
 set :gh_add_label, "repos/:owner/:repo/issues/:number/labels" # post
 set :gh_remove_label, "repos/:owner/:repo/issues/:number/labels/:name" # delete
 set :gh_update_label, "repos/:owner/:repo/issues/:number/labels" # put
-set :gh_edit_issue, "repos/:owner/:repo/issues/:number"
+set :gh_edit_issue, "repos/:owner/:repo/issues/:number" # patch
 
 
 def get_labels(user, repo, issue)
@@ -37,57 +39,57 @@ end
 
 def add_labels(user, repo, issue, label)
     endpoint = options.gh_add_label.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue)
-    curl = Curl::Easy.http_post(options.gh_api + endpoint, label) do |c|
+    Curl::Easy.http_post(options.gh_api + endpoint, label) do |c|
         c.http_auth_types = :basic
         c.username = options.gh_user
         c.password = options.gh_token
         c.headers['User-Agent'] = 'Github-Issuehooks'
     end
-    p curl.body_str
+    p 'added label(s)'
 end
 
 def remove_label(user, repo, issue, label)
     endpoint = options.gh_remove_label.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue).gsub(':name', label)
-    curl = Curl::Easy.http_delete(options.gh_api + endpoint) do |c|
+    Curl::Easy.http_delete(options.gh_api + endpoint) do |c|
         c.http_auth_types = :basic
         c.username = options.gh_user
         c.password = options.gh_token
         c.headers['User-Agent'] = 'Github-Issuehooks'
     end
-    p curl.body_str
+    p 'removed label(s)'
 end
 
 def update_labels(user, repo, issue, label)
     endpoint = options.gh_update_label.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue)
-    curl = Curl::Easy.http_put(options.gh_api + endpoint, label) do |c|
+    Curl::Easy.http_put(options.gh_api + endpoint, label) do |c|
         c.http_auth_types = :basic
         c.username = options.gh_user
         c.password = options.gh_token
         c.headers['User-Agent'] = 'Github-Issuehooks'
     end
-    p curl.body_str
+    p 'updated labels'
 end
 
 def assign_issue(user, repo, issue, assignee)
     endpoint = options.gh_edit_issue.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue)
-    curl = Curl::Easy.http_patch(options.gh_api + endpoint, {:assignee => assignee}.to_json) do |c|
+    Curl.patch(options.gh_api + endpoint, {:assignee => assignee}.to_json) do |c|
         c.http_auth_types = :basic
         c.username = options.gh_user
         c.password = options.gh_token
         c.headers['User-Agent'] = 'Github-Issuehooks'
     end
-    p curl.body_str
+    p 'assigned issue to ' + assignee
 end
 
 def assign_milestone(user, repo, issue, milestone)
     endpoint = options.gh_edit_issue.gsub(':owner', user).gsub(':repo', repo).gsub(':number', issue)
-    curl = Curl::Easy.http_patch(options.gh_api + endpoint, {:milestone => milestone}.to_json) do |c|
+    Curl.patch(options.gh_api + endpoint, {:milestone => milestone}.to_json) do |c|
         c.http_auth_types = :basic
         c.username = options.gh_user
         c.password = options.gh_token
         c.headers['User-Agent'] = 'Github-Issuehooks'
     end
-    p curl.body_str
+    p 'assigned new milestone'
 end
 
 get '/test' do
@@ -95,7 +97,9 @@ get '/test' do
 end
 
 post '/' do
-    push = JSON.parse(request.body.read)
+    request.body.rewind
+    json = request.body.read.to_s
+    push = json && json.length >= 2 ? JSON.parse(json) : nil
     repo = push['repository']['name']
     owner = push['repository']['owner']['name']
     push['commits'].each do |c|
@@ -104,12 +108,8 @@ post '/' do
         if issue.size == 1 #only check for other goodies if an issue is mentioned
             
             # user assignment
-            begin
-                user = m.scan(/\=[a-zA-Z0-9]+/)[0].split(//)[1..-1].join
-                assign_issue(owner, repo, issue[0], user)
-            rescue => e
-              p e.to_s
-            end
+            user = m.scan(/\=[a-zA-Z0-9\-]+/)[0] # hyphens and alphanumerics are allowed in github usernames
+            assign_issue(owner, repo, issue[0], user[1..-1]) if user
             
             # label toggles
             label_toggles = m.scan(/\~[a-zA-Z0-9]+|\~".*?"|\~'.*?'/)
@@ -136,12 +136,10 @@ post '/' do
             end
 
             # milestone assignment
-            begin
-                milestone = m.scan(/\^[a-zA-Z0-9]+/)[0].split(//)[1..-1].join
-                assign_milestone(owner, repo, issue[0], milestone)
-            rescue => e
-              p e.to_s
-            end
+            milestone = m.scan(/\^[0-9]+/)[0]
+            assign_milestone(owner, repo, issue[0], milestone[1..-1]) if milestone
         end
     end
+    status 200
+    body ''
 end
